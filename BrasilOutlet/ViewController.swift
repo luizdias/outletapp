ea558ad691b7c0f97a4367ff5d32832b4582af82
Luiz Dias <luizfad@gmail.com>
2015-10-22 23:15:49 -0200

    Initial Commit

---
+++ BrasilOutlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrasilOutlet
+//
+//  Created by Luiz Dias on 10/22/15.
+//  Copyright © 2015 Luiz Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
