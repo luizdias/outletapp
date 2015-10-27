80c5358ebb8c9623bcdc6f9257b5ef137c28c289
Luiz Dias <luizfad@gmail.com>
2015-10-27 04:05:00 -0200

    Commit Inicial do app Cliente com listagem de produtos.

---
+++ BrasilOutlet/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  BrasilOutlet
+//
+//  Created by Luiz Dias on 10/27/15.
+//  Copyright © 2015 Luiz Dias. All rights reserved.
+//
+
+import Foundation
---
+++ BrasilOutlet/ProductsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsCollectionViewCell.swift
+//  BrasilOutlet
+//
+//  Created by Luiz Dias on 10/26/15.
+//  Copyright © 2015 Luiz Dias. All rights reserved.
+//
+
+import UIKit
+
+class ProductsCollectionViewCell: UICollectionViewCell {
+    
+}
