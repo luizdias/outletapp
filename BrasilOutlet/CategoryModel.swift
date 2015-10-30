e3c39f38169e28ac8e0a47a9a65be910cad9384f
Luiz Dias <luizfad@gmail.com>
2015-10-30 03:06:22 -0200

    Criação da Tab Bar e listagem de categorias.

---
+++ BrasilOutlet/CategoryModel.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesModel.swift
+//  BrasilOutlet
+//
+//  Created by Luiz Dias on 10/30/15.
+//  Copyright © 2015 Luiz Dias. All rights reserved.
+//
+
+import Foundation
+
+class CategoriesModel {
+    
+    var id = "0"
+    var name = " "
+    var favorite = "1"
+    var gender = " "
+    var idfather = "2"
+    var image_path = " "
+    var image = " "
+    
+}
